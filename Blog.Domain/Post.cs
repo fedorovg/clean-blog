182d98e6497e996fe8ce6ac7f5fe16b17c46d418
Gleb Fedorov <fedorgle@fit.cvut.cz>
2021-10-08 14:21:21 +0200

    Added Post class

---
+++ Blog.Domain/Post.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Blog.Domain
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public int UserId { get; set; }
+    }
+}
