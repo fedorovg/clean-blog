470f7ebf5120a083a5671a7379ec29f5c17b1287
Gleb Fedorov <fedorgle@fit.cvut.cz>
2021-10-08 21:08:30 +0200

    Added a command to create posts

---
+++ Blog.Application/Posts/Commands/CreatePost.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Blog.Application.Interfaces;
+using Blog.Domain;
+using MediatR;
+
+namespace Blog.Application.Posts.Commands
+{
+    public class CreatePostCommand : IRequest<int>
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int UserId { get; set; }
+    }
+
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, int>
+    {
+        private readonly IBlogDbContext _context;
+
+        public CreatePostCommandHandler(IBlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            var post = new Post()
+            {
+                Title = request.Title,
+                Content = request.Content,
+                UserId = request.UserId,
+                Created = DateTime.Now,
+                Updated = null
+            };
+            await _context.Posts.AddAsync(post, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return post.Id;
+        }
+    }
+}
