40abfab51e0024dc98fa902345775efe74e37e6e
Gleb Fedorov <fedorgle@fit.cvut.cz>
2021-10-10 00:17:35 +0200

    Added fluent validation to the mediatr pipeline

--- Blog.Application/DependencyInjection.cs
+++ Blog.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using Blog.Application.Common.Behaviours;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using MediatR;
 
@@ -8,7 +10,9 @@ namespace Blog.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddMediatR(Assembly.GetExecutingAssembly());            
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             return services;
         }
     }
