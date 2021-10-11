aef3d88e3ad4a19c871978831d9452803a673a81
Gleb Fedorov <fedorgle@fit.cvut.cz>
2021-10-11 02:45:55 +0200

    Added an action to the Post controller to get related comments

--- Blog.Api/Controllers/PostController.cs
+++ Blog.Api/Controllers/PostController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using AutoMapper;
 using Blog.Api.Models.Posts;
+using Blog.Application.Comments.Queries;
 using Blog.Application.Posts.Commands;
 using Blog.Application.Posts.Queries;
 using MediatR;
@@ -10,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Blog.Api.Controllers
 {
     [ApiController, Route("api/[controller]")]
-    public class PostController : ControllerBase
+    public class PostController : BaseController
     {
         private readonly IMapper _mapper;
         private readonly IMediator _mediator;
@@ -22,12 +23,9 @@ namespace Blog.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<PostDetailsVm>> GetAllPosts()
+        public async Task<ActionResult<PostListVm>> GetAllPosts()
         {
-            var query = new GetPostListQuery()
-            {
-                UserId = 1
-            };
+            var query = new GetPostListQuery();
             var posts = await _mediator.Send(query);
             return Ok(posts);
         }
@@ -35,16 +33,27 @@ namespace Blog.Api.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult<PostDetailsVm>> GetPostDetails(int id)
         {
-            var query = new GetPostDetailsQuery() {Id = id, UserId = Guid.Empty};
+            var query = new GetPostDetailsQuery() {Id = id, UserId = UserId};
             var postDetails = await _mediator.Send(query);
             return Ok(postDetails);
+        }
+
+        [HttpGet("{id:int}/comments")]
+        public async Task<ActionResult> GetPostComments(int id)
+        {
+            var query = new GetPostCommentsQuery()
+            {
+                PostId = id
+            };
+            var comments = await _mediator.Send(query);
+            return Ok(comments);
         }
 
         [HttpPost]
         public async Task<IActionResult> CreatePost([FromBody] CreatePostDto createPostDto)
         {
             var command = _mapper.Map<CreatePostDto, CreatePostCommand>(createPostDto);
-            command.UserId = Guid.Empty;
+            command.UserId = UserId;
             var createdPostId = await _mediator.Send(command);
             return Created(Url.Action(nameof(GetPostDetails), new {id = createdPostId}), createdPostId);
         }
@@ -54,7 +63,7 @@ namespace Blog.Api.Controllers
         {
             var command = _mapper.Map<UpdatePostDto, UpdatePostCommand>(updatePostDto);
             command.Id = id;
-            command.UserId = Guid.Empty;
+            command.UserId = UserId;
             await _mediator.Send(command);
             return NoContent();
         }
@@ -62,7 +71,7 @@ namespace Blog.Api.Controllers
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> DeletePost(int id)
         {
-            var command = new DeletePostCommand() {Id = id, UserId = Guid.Empty};
+            var command = new DeletePostCommand() {Id = id, UserId = UserId};
             await _mediator.Send(command);
             return NoContent();
         }
